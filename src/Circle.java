d9ff6c0bda5307a162028398fd9aa445008db53f
Kamal_Agarwal <Kamal_Agarwal@EPUSPRIW00B1.princeton.epam.com>
2022-07-30 08:16:12 -0700

    Changes in Logic

--- src/Circle.java
+++ src/Circle.java
@@ -4,10 +4,10 @@ public class Circle {
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
-        System.out.print("Input the Radius.. ");
-        int radiusOfCircle = in.nextInt(); 
-        double Perimeter =  2 * 3.14 * radiusOfCircle;
-        double Area = 3.14 * radiusOfCircle * radiusOfCircle;
+        System.out.print("Input the Diameter.. ");
+        int DiameterOfCircle = in.nextInt(); 
+        double Perimeter =  2 * 3.14 * DiameterOfCircle;
+        double Area = 3.14 * DiameterOfCircle * DiameterOfCircle;
         System.out.println("Perimeter is = " + (Perimeter));
         System.out.println("Area is = " + (Area));
 	}
